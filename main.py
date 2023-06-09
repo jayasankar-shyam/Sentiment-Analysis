defa6a9aeda78cb1420f5a7e9b2d52876f070913
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2023-06-09 19:37:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI,Request
 from fastapi.middleware.cors import CORSMiddleware
+import pickle
+import keras
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+# Load the configurations from the file
+with open('countvectorizer_config.pkl', 'rb') as f:
+    configurations = pickle.load(f)
 
+# Create a new CountVectorizer object with the saved configurations
+vectorizer = CountVectorizer(vocabulary=configurations['vocabulary'],
+                             stop_words=configurations['stop_words'],
+                             binary=configurations['binary'])
+model = keras.models.load_model("k.h5")
 app = FastAPI()
 
 # Enable CORS to allow the webpage to make requests to the FastAPI server
@@ -14,12 +26,15 @@ app.add_middleware(
 async def check_character_count(request: Request):
     data = await request.json()
     comment = data.get('comment', '')
-    if len(comment) < 10:
-        return {'result': 1}
-    else:
+    comment = pd.Series(comment)
+    comment = vectorizer.transform(comment)
+    p=model.predict(comment)
+    if p[0] > 0.5:
         return {'result': 0}
+    else:
+        return {'result': 1}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-#python -m uvicorn main:app --reload
+#cd D:/coding/sentiment-analysis && python -m uvicorn main:app --reload
 
