25d31cf9b6ddca9196838800abe2e55b5efb3b02
jayasankar-shyam <jayasankar06shyam@gmail.com>
2023-06-11 17:21:27 +0530

    a

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["http://192.168.0.170:5500", "https://jayasankar-shyam.github.io/Sentiment-Analysis", "https://comment-moderation.netlify.app"]}})
 # Load the Keras model
 model = load_model('k.h5')
-
 # Load the CountVectorizer configuration
 with open('countvectorizer_config.pkl', 'rb') as file:
     count_vectorizer_config = pickle.load(file)
-
-# Initialize the CountVectorizer
 count_vectorizer = CountVectorizer(**count_vectorizer_config)
 
 @app.route('/', methods=['GET','POST'])
@@ -20,13 +17,10 @@ def predict():
     if request.method == "POST":
         # Get the comment from the request
         comment = request.json['comment']
-
         # Vectorize the comment
         comment_vector = count_vectorizer.transform([comment])
-
-        # Make a prediction using the loaded model
+        # Make prediction using the loaded model
         prediction = model.predict(comment_vector)[0][0]
-
         # Return the result based on the prediction
         if prediction > 0.5:
             result = {'result': 0}
